170245d82d16b84f2e688d2552a4a77b3b37b0a5
Son Pham <phamiosvn@gmail.com>
2015-06-25 09:18:23 +0700

    Initial Commit

---
+++ WorldClock/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  WorldClock
+//
+//  Created by DungLT on 6/25/15.
+//  Copyright (c) 2015 DungLT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ WorldClock/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  WorldClock
+//
+//  Created by DungLT on 6/25/15.
+//  Copyright (c) 2015 DungLT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
